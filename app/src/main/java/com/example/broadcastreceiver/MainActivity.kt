e78c7adb74d240abc2051ef29e7272ee1013d95f
татьяна <emuart155@gmail.com>
2021-12-12 12:12:54 +0300

    Динамическая регистрация в коде - режим самолета. Статическая регистрация в манифесте - смена языка.

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.broadcastreceiver
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Регистрируем BroadcastReceiver программно(ни в манифесте).
+ * Это называется динамическая регистрация.
+ * Для этого мы просто создаем
+ * экземпляр нашего ресивера и регистрируем его в MainActivity
+ */
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = MainBroadcastReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * регистрируем ресивер через метод registerReceiver, который принимает
+        наш ресивер и интент-фильтр с указанием, на какое сообщение ОС хотим подписаться. Например, на
+        переход в режим самолёта Intent.ACTION_AIRPLANE_MODE_CHANGED.
+         */
+        registerReceiver(
+            receiver,
+            IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        )
+    }
+
+    override fun onDestroy() {
+
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 }
